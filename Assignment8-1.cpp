41121ddc11f34e4813caff4e8926218035b5edec
Kyuwoong Lee <kwooler@gmail.com>
2021-09-14 22:50:18 -0700

    Update Assignment8-1.cpp
    Refer to the comments inside the file.

--- Assignment8-1.cpp
+++ Assignment8-1.cpp
@@ -12,9 +12,12 @@ int main()
   int index;
 
 index = binarySearch (numbers,N,target);
+	// this function always returns -1
+	
 if (index == -1)
   cout << "not found\n";
-else
+else 
+	// This else never be executed
   cout << "found at index : " << index << endl;
 }
 int binarySearch(int number[], int N, int target)
@@ -38,6 +41,7 @@ int binarySearch(int number[], int N, int target)
   }
 
   return -1;
+	// you have only one return statement with the value -1
 }
 
 
