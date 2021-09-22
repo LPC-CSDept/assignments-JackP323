3100122c130ccb21371c825029f8f08d63645441
John Patten <89234309+JackP323@users.noreply.github.com>
2021-09-22 02:44:08 +0000

    fixed binary

--- AB-2.cpp
+++ AB-2.cpp
@@ -41,19 +41,26 @@ int main()
 
 	// call binarySearch TRY times
 
-  int binarySearch(int array[],int N,int target);
-
-{   int first, int last, int mid;
-
+  int binarySearch(int array[], int N, int target)
+{
+    int first, last, mid;
     int cmp = 0;
+
     first = 0;
     last = N-1;
-    
-
-    while (first <= last) {
-      mid = (first + last) / 2;
-      
+    while ( first <= last) 
+    {
+		cmp += 1;
+        mid = (first + last) / 2;
+        if ( array[mid] == target)
+            return cmp;
+        if ( array[mid] > target)
+            last = mid - 1;
+        else 
+            first = mid + 1;
     }
+    return cmp;
+}
 
 
     
